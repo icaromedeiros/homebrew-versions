0b57943ae5e95a6fa1a0c0bf5fa605370b089200
Icaro Medeiros <icaro.medeiros@gmail.com>
2014-10-20 14:06:57 -0200

    Virtuoso 6.1.6 formula

---
+++ virtuoso616.rb
@@ -0,0 +1,36 @@
+require 'formula'
+
+class Virtuoso616 < Formula
+  homepage 'http://virtuoso.openlinksw.com/wiki/main/'
+  url "https://downloads.sourceforge.net/project/virtuoso/virtuoso/6.1.6/virtuoso-opensource-6.1.6.tar.gz"
+  sha1 "03bc14b1627d16d76687f8b8659801966aab3fb4"
+
+  head do
+    url 'https://github.com/openlink/virtuoso-opensource.git', :branch => 'develop/6'
+
+    depends_on :autoconf
+    depends_on :automake
+    depends_on :libtool
+  end
+
+  # If gawk isn't found, make fails deep into the process.
+  depends_on 'gawk' => :build
+
+  conflicts_with 'unixodbc', :because => 'Both install `isql` binaries.'
+
+  skip_clean :la
+
+  def install
+    ENV.m64 if MacOS.prefer_64_bit?
+    system "./autogen.sh" if build.head?
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make install"
+  end
+
+  def caveats; <<-EOS.undent
+    NOTE: the Virtuoso server will start up several times on port 1111
+    during the install process.
+    EOS
+  end
+end
