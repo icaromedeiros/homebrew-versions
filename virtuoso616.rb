b6b29ea07ad85d257f68fbe1a7ed3329534f2890
Icaro Medeiros <icaro.medeiros@gmail.com>
2015-02-01 17:32:45 -0200

    fix style and errors pointed out by brew audit

--- virtuoso616.rb
+++ virtuoso616.rb
@@ -1,12 +1,10 @@
-require 'formula'
-
 class Virtuoso616 < Formula
-  homepage 'http://virtuoso.openlinksw.com/wiki/main/'
+  homepage "http://virtuoso.openlinksw.com/wiki/main/"
   url "https://downloads.sourceforge.net/project/virtuoso/virtuoso/6.1.6/virtuoso-opensource-6.1.6.tar.gz"
   sha1 "03bc14b1627d16d76687f8b8659801966aab3fb4"
 
   head do
-    url 'https://github.com/openlink/virtuoso-opensource.git', :branch => 'develop/6'
+    url "https://github.com/openlink/virtuoso-opensource.git", :branch => "develop/6"
 
     depends_on "autoconf" => :build
     depends_on "automake" => :build
@@ -14,10 +12,10 @@ class Virtuoso616 < Formula
   end
 
   # If gawk isn't found, make fails deep into the process.
-  depends_on 'gawk' => :build
+  depends_on "gawk" => :build
   depends_on "openssl"
 
-  conflicts_with 'unixodbc', :because => 'Both install `isql` binaries.'
+  conflicts_with "unixodbc", :because => "Both install `isql` binaries."
 
   skip_clean :la
 
@@ -26,12 +24,16 @@ class Virtuoso616 < Formula
     system "./autogen.sh" if build.head?
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
   end
 
   def caveats; <<-EOS.undent
     NOTE: the Virtuoso server will start up several times on port 1111
     during the install process.
     EOS
+  end
+
+  test do
+    "[[ $(#{bin}/virtuoso-t --help) != *6.1.6* ]]"
   end
 end
